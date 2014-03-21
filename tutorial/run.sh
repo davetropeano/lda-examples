6c9fd5d14563d97883942dae493aeeb5ac436b3f
frankbu <frankb@ca.ibm.com>
2014-03-21 13:40:56 -0400

    initial src

---
+++ tutorial/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PYTHONPATH=.:../../lda-mongodbstorage:../../lda-serverlib
+python test_server.py
---
+++ tutorial/test_server.py
@@ -0,0 +1,28 @@
+import os, logging
+from wsgiref.simple_server import make_server
+from wsgiref.simple_server import WSGIServer
+from SocketServer import ThreadingMixIn
+
+class ThreadedWSGIServer(ThreadingMixIn, WSGIServer):
+    """Handle requests in a separate thread."""
+
+APP_NAME = 'tutorial'
+PORT = 3007
+    
+os.environ['APP_NAME'] = APP_NAME
+os.environ['MONGODB_DB_HOST'] = 'localhost' # 'ec2-50-19-190-34.compute-1.amazonaws.com'
+os.environ['MONGODB_DB_PORT'] = '27017'
+os.environ['CONCIERGERIE_HOST'] = 'localhost:%s' % PORT # 'ec2-50-19-190-34.compute-1.amazonaws.com'
+ 
+from logic_server import application
+from werkzeug.wsgi import SharedDataMiddleware
+
+application = SharedDataMiddleware(application, {
+    '/clientlib': os.path.join(os.path.dirname(__file__), '../../lda-clientlib'),
+    '/tutorial': os.path.join(os.path.dirname(__file__), 'static'),
+})
+
+httpd = make_server('0.0.0.0', PORT, application, server_class=ThreadedWSGIServer)
+print '%s application initiated on host: localhost port: %d' % (APP_NAME, PORT)
+logging.basicConfig(level=logging.DEBUG)
+httpd.serve_forever()
