9476cb81cc0e60b600019fe41c84824be3e52ef0
davetropeano <davetropeano@yahoo.com>
2014-04-23 11:41:34 -0400

    sh script changes for osx. commented out header problem in ce_account.html

--- setupshop/test/setupshop_test_data_create.sh
+++ setupshop/test/setupshop_test_data_create.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-set PYTHONPATH=.:../src:../../../lda-clientlib/python:../../../lda-clientlib/python/test:%PYTHONPATH
-python setupshop_test_data_create.py "$1"
+export PYTHONPATH=.:../src:../../../lda-clientlib/python:../../../lda-clientlib/python/test:%PYTHONPATH
+python setupshop_test_data_create.py $1
