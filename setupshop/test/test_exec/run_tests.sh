a0b8d2ba9609ac12a93a2fb6cd54b75ee7a6629b
Mark Archer <marcher@uci.edu>
2015-01-15 07:46:13 -0800

    work to test all the examples instead of just setupshop

---
+++ setupshop/test/test_exec/run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#TODO: set location to lda-examples root via ~
+pwd
+cd ../../..
+
+# Copy nginx config and start service
+sudo cp -f dispatcher.conf /etc/nginx/conf.d/dispatcher.conf
+sudo service nginx restart
+
+# start setupshop server and direct output to log file
+cd setupshop
+nohup sh run.sh > ~/setupshop_server_test.log &
+
+# start siteserver and direct output to log file
+cd ../../lda-siteserver
+nohup sh run.sh > ~/site_server_test.log &
+
+# wait for servers to come up
+sleep 10
+
+# create test data for setupshop
+cd test;
+sh test_data_create.sh;
+
+# create test data for siteserver
+cd ../../lda-examples/setupshop/test;
+sh setupshop_test_data_create.sh;
+
+# execute tests
+cd test_exec;
+py.test
+
+# TODO: kill servers
+
+# output siteserver log
+cd ../..
+cat ~/site_server_test.log
+
+# output setupshot log
+cd ../../lda-examples
+cat ~/setupshop_server_test.log
+
